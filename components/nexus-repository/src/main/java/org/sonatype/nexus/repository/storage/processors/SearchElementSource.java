819f6e485190ebf5d43543727b98b6d99254a863
Tamas Cservenak <tamas@cservenak.net>
2015-04-21 16:48:36 +0200

    Added example of search based element source

---
+++ components/nexus-repository/src/main/java/org/sonatype/nexus/repository/storage/processors/SearchElementSource.java
@@ -0,0 +1,35 @@
+package org.sonatype.nexus.repository.storage.processors;
+
+import org.sonatype.nexus.repository.search.SearchService;
+import org.sonatype.nexus.repository.storage.ProcessorContext;
+
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.search.SearchHit;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * Element source that uses {@link SearchService} and {@link SearchHit} as elements.
+ *
+ * @since 3.0
+ */
+public class SearchElementSource
+    extends ValueElementSource<SearchHit>
+{
+  private final SearchService searchService;
+
+  private final QueryBuilder queryBuilder;
+
+  public SearchElementSource(final SearchService searchService,
+                             final QueryBuilder queryBuilder)
+  {
+    super(SearchHit.class);
+    this.searchService = checkNotNull(searchService);
+    this.queryBuilder = checkNotNull(queryBuilder);
+  }
+
+  @Override
+  public Iterable<SearchHit> elements(final ProcessorContext context) {
+    return searchService.browse(queryBuilder);
+  }
+}
