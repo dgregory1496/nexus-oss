d82a297818f16f7466f9017578de8eae5d83e74a
Tamas Cservenak <tamas@cservenak.net>
2015-04-21 17:17:30 +0200

    Remove redundant "repack", element sources does it

--- components/nexus-repository/src/main/java/org/sonatype/nexus/repository/storage/processors/Loop.java
+++ components/nexus-repository/src/main/java/org/sonatype/nexus/repository/storage/processors/Loop.java
@@ -2,15 +2,8 @@ package org.sonatype.nexus.repository.storage.processors;
 
 import java.util.List;
 
-import javax.annotation.Nullable;
-
-import org.sonatype.nexus.repository.storage.Component;
 import org.sonatype.nexus.repository.storage.Processor;
 import org.sonatype.nexus.repository.storage.ProcessorContext;
-import org.sonatype.nexus.repository.storage.StorageTx;
-
-import com.google.common.collect.Iterables;
-import com.google.common.collect.Lists;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
@@ -30,20 +23,11 @@ public class Loop<T>
 
   @Override
   public void process(final ProcessorContext context) {
-    final List<T> elements = elements(context);
+    final Iterable<T> elements = elementSource.elements(context);
     for (T element : elements) {
       elementSource.addToContext(context, element);
       context.proceed();
       elementSource.removeFromContext(context, element);
     }
-  }
-
-  private List<T> elements(final ProcessorContext context) {
-    final List<T> elements = Lists.newArrayList();
-    Iterables.addAll(
-        elements,
-        elementSource.elements(context)
-    );
-    return elements;
   }
 }
