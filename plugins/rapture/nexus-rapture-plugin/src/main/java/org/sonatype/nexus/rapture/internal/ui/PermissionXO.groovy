f26971a7fc50b53da0b5e564e36b56c72fa1b063
Alin Dreghiciu <adreghiciu@gmail.com>
2015-04-27 11:59:12 +0300

    Fix imports

--- plugins/rapture/nexus-rapture-plugin/src/main/java/org/sonatype/nexus/rapture/internal/ui/PermissionXO.groovy
+++ plugins/rapture/nexus-rapture-plugin/src/main/java/org/sonatype/nexus/rapture/internal/ui/PermissionXO.groovy
@@ -13,7 +13,6 @@
 package org.sonatype.nexus.rapture.internal.ui
 
 import groovy.transform.ToString
-import org.apache.shiro.authz.permission.WildcardPermission
 
 /**
  * Permission exchange object.
